0f6696085c252caf169a1bc2e27ecc751b651743
Andrew Oplas <andrewmoplas@gmail.com>
2023-09-05 19:04:35 +0800

    add module

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<React.StrictMode />, document.getElementById("root"));
