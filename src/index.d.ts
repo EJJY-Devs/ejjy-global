630fe9b975d3a4806048e6de2e30fa821482f683
Andrew Oplas <andrewmoplas@gmail.com>
2023-09-05 19:35:36 +0800

    update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,1 @@
-declare module "*.module.css" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
-declare module "*.module.scss" {
-  const classes: { readonly [key: string]: string };
-  export default classes;
-}
-
 export * from "./types";
