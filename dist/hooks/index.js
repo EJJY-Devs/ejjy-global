89d05d97451b05d96f6c46cd689189c5d2481f21
Andrew Oplas <andrewmoplas@gmail.com>
2023-09-07 02:20:00 +0800

    add useAccounts to export

--- dist/hooks/index.js
+++ dist/hooks/index.js
@@ -3,6 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.useBranches = void 0;
+exports.useBranches = exports.useAccounts = void 0;
+var useAccounts_1 = require("./useAccounts");
+Object.defineProperty(exports, "useAccounts", { enumerable: true, get: function () { return __importDefault(useAccounts_1).default; } });
 var useBranches_1 = require("./useBranches");
 Object.defineProperty(exports, "useBranches", { enumerable: true, get: function () { return __importDefault(useBranches_1).default; } });
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,2 @@
+export { default as useAccounts } from './useAccounts';
 export { default as useBranches } from './useBranches';
