e591cafc34f41a68b76497fff40ba35592ceea7b
Andrew Oplas <andrewmoplas@gmail.com>
2024-01-24 23:44:53 +0800

    add connect service

--- dist/services/ScaleService.d.ts
+++ dist/services/ScaleService.d.ts
@@ -1,4 +1,8 @@
+export interface Connect {
+    comPort: number;
+}
 declare const service: {
+    connect: (body: Connect) => Promise<import("axios").AxiosResponse<boolean>>;
     getWeight: (baseURL: string) => Promise<number>;
 };
 export default service;
--- src/services/ScaleService.ts
+++ src/services/ScaleService.ts
@@ -1,6 +1,12 @@
 import axios from 'axios';
 
+export interface Connect {
+	comPort: number;
+}
+
 const service = {
+	connect: async (body: Connect) => axios.post<boolean>('/connect/', body),
+
 	getWeight: async (baseURL: string) => {
 		const response = await axios.get<number>('/weight', { baseURL });
 
