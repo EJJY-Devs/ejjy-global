bb194fa310d6d86e4f3ddcd36c02d732306556b3
Andrew Oplas <andrewmoplas@gmail.com>
2024-03-09 23:22:27 +0800

    update build

--- dist/print/configureTxt.d.ts
+++
@@ -1,5 +0,0 @@
-import { BranchMachine, DailySales, GeneratedByUser, SiteSettings, Transaction, XReadReport, ZReadReport } from '../types';
-export declare const createSalesInvoiceTxt: (transaction: Transaction, siteSettings: SiteSettings, branchMachine: BranchMachine, isReprint?: boolean, returnContent?: boolean) => string | null;
-export declare const createXReadTxt: (report: XReadReport, siteSettings: SiteSettings, branchMachine: BranchMachine, user: GeneratedByUser, returnContent?: boolean) => string | null;
-export declare const createDailySalesTxt: (dailySales: DailySales, siteSettings: SiteSettings, branchMachine: BranchMachine, user: GeneratedByUser) => null;
-export declare const createZReadTxt: (report: ZReadReport, siteSettings: SiteSettings, branchMachine: BranchMachine, user: GeneratedByUser, returnContent?: boolean) => string | null;
