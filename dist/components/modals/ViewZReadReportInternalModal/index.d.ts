97e819bf484efdc320fae26e14800a693fc33728
Andrew Oplas <andrewmoplas@gmail.com>
2024-03-13 23:30:25 +0800

    update dependencies

--- dist/components/modals/ViewZReadReportInternalModal/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { SiteSettings, ZReadReport } from '../../../types';
-interface Props {
-    report: ZReadReport;
-    siteSettings: SiteSettings;
-    onClose: () => void;
-}
-export declare const ViewZReadReportModal: ({ report, siteSettings, onClose, }: Props) => React.JSX.Element;
-export {};
