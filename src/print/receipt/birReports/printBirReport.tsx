94124fc02b5849764bfc870e2ad4a41b8428f17b
Andrew Oplas <andrewmoplas@gmail.com>
2024-04-21 01:41:26 +0800

    update bir report

--- src/print/receipt/birReports/printBirReport.tsx
+++ src/print/receipt/birReports/printBirReport.tsx
@@ -16,6 +16,7 @@ export const printBirReport = (
 			<td>{formatDate(report.date)}</td>
 			<td>{report?.beginning_or?.or_number || EMPTY_CELL}</td>
 			<td>{report?.ending_or?.or_number || EMPTY_CELL}</td>
+
 			<td>
 				{formatInPeso(report.grand_accumulated_sales_ending_balance, PESO_SIGN)}
 			</td>
@@ -25,31 +26,36 @@ export const printBirReport = (
 					PESO_SIGN,
 				)}
 			</td>
-			<td>{formatInPeso(report.gross_sales_for_the_day, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.sales_issue_with_manual, PESO_SIGN)}</td>
-			<td>{formatInPeso(report.gross_sales_from_pos, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.gross_sales_for_the_day, PESO_SIGN)}</td>
+
 			<td>{formatInPeso(report.vatable_sales, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.vat_amount, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.vat_exempt_sales, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.zero_rated_sales, PESO_SIGN)}</td>
 
-			<td>{formatInPeso(report.regular_discount, PESO_SIGN)}</td>
-			<td>{formatInPeso(report.special_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.sc_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.pwd_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.naac_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.sp_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.others_discount, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.returns, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.void, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.total_deductions, PESO_SIGN)}</td>
 
-			<td>{formatInPeso(report.vat_on_special_discounts, PESO_SIGN)}</td>
-			<td>{formatInPeso(report.vat_on_returns, PESO_SIGN)}</td>
-			<td>{formatInPeso(report.others, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.vat_sc_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.vat_pwd_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.vat_others_discount, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.vat_returns, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.vat_others, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.total_vat_adjusted, PESO_SIGN)}</td>
 
 			<td>{formatInPeso(report.vat_payable, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.net_sales, PESO_SIGN)}</td>
-			<td>{formatInPeso(report.other_income, PESO_SIGN)}</td>
 			<td>{formatInPeso(report.sales_overrun_or_overflow, PESO_SIGN)}</td>
-			<td>{formatInPeso(report.total_net_sales, PESO_SIGN)}</td>
+			<td>{formatInPeso(report.total_income, PESO_SIGN)}</td>
 			<td>{report.reset_counter}</td>
+			<td>{report.z_counter}</td>
 			<td>{report.remarks}</td>
 		</tr>
 	));
@@ -69,40 +75,55 @@ export const printBirReport = (
 
 					<table className="bir-reports">
 						<tr>
-							<th rowSpan={2}>Date</th>
-							<th rowSpan={2}>Beginning SI/OR No.</th>
-							<th rowSpan={2}>Ending SI/OR No. </th>
-							<th rowSpan={2}>Grand Accum. Sales Ending Balance</th>
-							<th rowSpan={2}>Grand Accum. Sales Beginning Balance</th>
-							<th rowSpan={2}>Gross Sales for the Day</th>
-							<th rowSpan={2}>
-								Sales Issued with Manual SI/OR (per RR 16-2018)
-							</th>
-							<th rowSpan={2}>Gross Sales From POS</th>
-							<th rowSpan={2}>VATable Sales</th>
-							<th rowSpan={2}>VAT Amount (12%)</th>
-							<th rowSpan={2}>VAT-Exempt Sales</th>
-							<th rowSpan={2}>Zero Rated Sales</th>
-							<th colSpan={5}>Deductions</th>
-							<th colSpan={4}>Adjustments on VAT</th>
-							<th rowSpan={2}>VAT Payable</th>
-							<th rowSpan={2}>Net Sales</th>
-							<th rowSpan={2}>Other Income</th>
-							<th rowSpan={2}>Sales Overrun/ Overflow</th>
-							<th rowSpan={2}>Total Net Sales</th>
-							<th rowSpan={2}>Reset Counter</th>
-							<th rowSpan={2}>Remarks</th>
+							<th rowSpan={3}>Date</th>
+							<th rowSpan={3}>Beginning SI/OR No.</th>
+							<th rowSpan={3}>Ending SI/OR No.</th>
+
+							<th rowSpan={3}>Grand Accum. Sales Ending Balance</th>
+							<th rowSpan={3}>Grand Accum. Sales Beg. Balance</th>
+							<th rowSpan={3}>Sales Issued w/ Manual SI/OR (per RR 16-2018)</th>
+							<th rowSpan={3}>Gross Sales for the Day</th>
+
+							<th rowSpan={3}>VATable Sales</th>
+							<th rowSpan={3}>VAT Amount</th>
+							<th rowSpan={3}>VAT-Exempt Sales</th>
+							<th rowSpan={3}>Zero Rated Sales</th>
+
+							<th colSpan={8}>Deductions</th>
+
+							<th colSpan={6}>Adjustments on VAT</th>
+							<th colSpan={3}>Discount</th>
+
+							<th rowSpan={3}>VAT Payable</th>
+							<th rowSpan={3}>Net Sales</th>
+							<th rowSpan={3}>Other Income</th>
+							<th rowSpan={3}>Sales Overrun/ Overflow</th>
+							<th rowSpan={3}>Total Net Sales</th>
+							<th rowSpan={3}>Reset Counter</th>
+							<th rowSpan={3}>Remarks</th>
 						</tr>
-						<tr className="nested-row" style={{ fontWeight: 'bold' }}>
-							<td>Regular Discount</td>
-							<td>Special Discount</td>
-							<td>Returns</td>
-							<td>Void</td>
-							<td>Total Deductions</td>
-							<td>VAT on Specials</td>
-							<td>VAT on Returns</td>
-							<td>Others </td>
-							<td>Total VAT Adj.</td>
+
+						<tr className="nested-row">
+							<th colSpan={5}>Discount</th>
+							<th rowSpan={2}>Returns</th>
+							<th rowSpan={2}>Void</th>
+							<th rowSpan={2}>Total Deductions</th>
+
+							<th colSpan={3}>Discount</th>
+							<th rowSpan={2}>VAT on Returns</th>
+							<th rowSpan={2}>Others</th>
+							<th rowSpan={2}>Total VAT Adjustment</th>
+						</tr>
+
+						<tr className="nested-row">
+							<td>SC</td>
+							<td>PWD</td>
+							<td>NAAC</td>
+							<td>Solo Parent</td>
+							<td>Others</td>
+							<td>SC</td>
+							<td>PWD</td>
+							<td>Others</td>
 						</tr>
 
 						{rows}
