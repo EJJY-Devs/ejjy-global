6b022538ba94789fab6f4412cd7438a8588f0c0d
Andrew Oplas <andrewmoplas@gmail.com>
2024-04-26 02:49:59 +0800

    refactor PrintDetails component

--- dist/components/Printing/PrintDetails.js
+++ dist/components/Printing/PrintDetails.js
@@ -7,10 +7,7 @@ exports.PrintDetails = void 0;
 const react_1 = __importDefault(require("react"));
 const dayjs_1 = __importDefault(require("dayjs"));
 const utils_1 = require("../../utils");
-const PrintDetails = ({ user }) => (react_1.default.createElement("div", { style: { display: 'flex' } },
-    "Print Details:",
-    ' ',
-    user && (react_1.default.createElement("div", { style: { display: 'flex', justifyContent: 'space-between' } },
-        (0, utils_1.formatDateTime)((0, dayjs_1.default)(), false), user === null || user === void 0 ? void 0 :
-        user.employee_id))));
+const PrintDetails = ({ user }) => (react_1.default.createElement("div", null,
+    "PDT: ",
+    user && `${(0, utils_1.formatDateTime)((0, dayjs_1.default)(), false)} - ${user === null || user === void 0 ? void 0 : user.employee_id}`));
 exports.PrintDetails = PrintDetails;
--- src/components/Printing/PrintDetails.tsx
+++ src/components/Printing/PrintDetails.tsx
@@ -8,13 +8,7 @@ type Props = {
 };
 
 export const PrintDetails = ({ user }: Props) => (
-	<div style={{ display: 'flex' }}>
-		Print Details:{' '}
-		{user && (
-			<div style={{ display: 'flex', justifyContent: 'space-between' }}>
-				{formatDateTime(dayjs(), false)}
-				{user?.employee_id}
-			</div>
-		)}
+	<div>
+		PDT: {user && `${formatDateTime(dayjs(), false)} - ${user?.employee_id}`}
 	</div>
 );
