b392e54751dd1bc134a20287df90b2a67471c921
Vincent James Maranga <vjmrnga@gmail.com>
2025-02-17 19:55:18 +0800

    Remove padding

--- src/print/receipt/printRequisitionSlip.ts
+++ src/print/receipt/printRequisitionSlip.ts
@@ -20,7 +20,7 @@ export const printRequisitionSlip = (
 	isPdf = false,
 ) => {
 	const data = `
-  <div className="container" style="${getPageStyle('padding: 24px;')}">
+  <div className="container" style="${getPageStyle()}">
   ${getHeader(siteSettings, undefined, 'REQUISITION SLIP')}
 
     <br />
