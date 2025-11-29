362b6cf71b7a490070734f74704050b8dcc7fe67
Vincent Maranga <vjmrnga@gmail.com>
2025-11-29 09:52:18 +0800

    Update printingProductPriceTag

--- dist/print/receipt/printProductPriceTag.js
+++ dist/print/receipt/printProductPriceTag.js
@@ -6,7 +6,7 @@ const helper_receipt_1 = require("../helper-receipt");
 const helper_receipt_2 = require("../helper-receipt");
 const printProductPriceTagHtml = (product, siteSettings, paperSettings) => {
     var _a;
-    const name = ((_a = product.price_tag_print_details) === null || _a === void 0 ? void 0 : _a.replace('\n', '<br/>')) || helper_receipt_1.EMPTY_CELL;
+    const name = ((_a = product.price_tag_print_details) === null || _a === void 0 ? void 0 : _a.replace(/\n/g, '<br/>')) || helper_receipt_1.EMPTY_CELL;
     const price = (0, utils_1.formatInPeso)(product.price_per_piece, helper_receipt_1.PESO_SIGN);
     return `
 	<div style="
@@ -15,14 +15,15 @@ const printProductPriceTagHtml = (product, siteSettings, paperSettings) => {
     padding: 1mm 2mm 0 2mm;
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     font-size: ${paperSettings.fontSize}px;
     font-family: ${paperSettings.fontFamily};
     line-height: 100%;
     color: black;
-    overflow:hidden;
+    overflow: hidden;
   ">
-    <div style="height: 2.2em; overflow: hidden; font-size: 1em; line-height: 1.1em;">${name}</div>
-    <div style="font-size: 1.53em; text-align: right;">${price}</div>
+    <div style="max-height: 3.3em; overflow: hidden; font-size: 1em; line-height: 1.1em; word-wrap: break-word; overflow-wrap: break-word;">${name}</div>
+    <div style="font-size: 1.53em; text-align: right; flex-shrink: 0; margin-top: auto;">${price}</div>
 	</div>
 	`;
 };
--- src/print/receipt/printProductPriceTag.ts
+++ src/print/receipt/printProductPriceTag.ts
@@ -16,7 +16,7 @@ export const printProductPriceTagHtml = (
 	paperSettings: PaperSettings,
 ) => {
 	const name =
-		product.price_tag_print_details?.replace('\n', '<br/>') || EMPTY_CELL;
+		product.price_tag_print_details?.replace(/\n/g, '<br/>') || EMPTY_CELL;
 	const price = formatInPeso(product.price_per_piece, PESO_SIGN);
 
 	return `
@@ -26,14 +26,15 @@ export const printProductPriceTagHtml = (
     padding: 1mm 2mm 0 2mm;
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     font-size: ${paperSettings.fontSize}px;
     font-family: ${paperSettings.fontFamily};
     line-height: 100%;
     color: black;
-    overflow:hidden;
+    overflow: hidden;
   ">
-    <div style="height: 2.2em; overflow: hidden; font-size: 1em; line-height: 1.1em;">${name}</div>
-    <div style="font-size: 1.53em; text-align: right;">${price}</div>
+    <div style="max-height: 3.3em; overflow: hidden; font-size: 1em; line-height: 1.1em; word-wrap: break-word; overflow-wrap: break-word;">${name}</div>
+    <div style="font-size: 1.53em; text-align: right; flex-shrink: 0; margin-top: auto;">${price}</div>
 	</div>
 	`;
 };
