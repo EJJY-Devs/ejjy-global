969ed5aa4b97561f3f892dcce58961614f47cb11
Vincent Maranga <vjmrnga@gmail.com>
2026-08-29 13:11:56 +0800

    Fix spacing of bir reports

--- src/print/receipt/birReports/birReportHelper.tsx
+++ src/print/receipt/birReports/birReportHelper.tsx
@@ -29,7 +29,6 @@ export const birReportStyles = React.createElement('style', {}, [
     table.bir-reports th,
     table.bir-reports .nested-row td {
       min-width: 60px;
-      line-height: 100%;
     }
 
     table.bir-reports th[colspan] {
@@ -45,6 +44,12 @@ export const birReportStyles = React.createElement('style', {}, [
     table.bir-reports td {
       border: 1px solid black;
       text-align: center;
+      vertical-align: middle;
+      padding: 6px 4px;
+      box-sizing: border-box;
+      overflow-wrap: break-word;
+      word-break: normal;
+      line-height: 1.3;
     }
 
     .bir-reports-pdf .title {
